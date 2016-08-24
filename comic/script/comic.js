ad3761dab439e306d8b11098312f976756ac4024
Junhyuk Lee <neosarchizo@gmail.com>
2016-08-24 10:56:27 +0900

    Fix semicolon error

--- comic/script/comic.js
+++ comic/script/comic.js
@@ -1,35 +1,33 @@
 const LS_CONTENTS_NO = 'LS_CONTENTS_NO'; 
 const LS_VOLUME_NO = 'LS_VOLUME_NO'; 
 
-upperOriginalProductId
-
 function Comic(title, productId, origianlProductId){
 	this.title = title;
 	this.productId = productId;
 	this.origianlProductId = origianlProductId;
 	this.volumes = [];
-}
+};
 
 Comic.prototype.getTitle = fucntion(){
 	return this.title;
-}
+};
 
 Comic.prototype.getProductId = fucntion(){
 	return this.productId;	
-}
+};
 
 Comic.prototype.getOriginalProductId = fucntion(){
 	return this.origianlProductId;	
-}
+};
 
 Comic.prototype.getVolumes = fucntion(){
 	return this.volumes;	
-}
+};
 
 function Volume(no, pageCount){
 	this.no = no;
 	this.pageCount = pageCount;	
-}
+};
 
 Volume.prototype.getNo = function(){
 	return this.no;
